1d1545fa70baba974daa830cda540dc80dce3baf
seeusoon <to.asmarkov@gmail.com>
2024-10-03 03:23:43 +0300

    add documents dao

--- cocoarag/dao/documents.py
+++ cocoarag/dao/documents.py
@@ -1,10 +1,73 @@
 # document.py
-import redis
-from entities.documents import Document
+import yaml
+from uuid import uuid4
+from box import Box
 
-class DocumentDAO:
-    def __init__(self, redis_host='localhost', redis_port=6379, redis_db=0):
-        self.redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
-    def add_document(self, document: Document):
-        self.redis_client.set(document.id, document.content)
+from cocoarag.models.documents import DocumentModel
+
+
+def _load_config(path="../configs/credits.yml") -> Box:
+    """ Load config and return it Box representation
+    """
+    with open(path, "r") as file:
+        data: dict = yaml.safe_load(file)
+
+    return Box(data)
+
+
+config = _load_config()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    api_key=config.embeddings_model.open_ai.token
+)
+
+connection = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"  # Uses psycopg3!
+
+
+# TODO: maybe add_chunks_dao?
+def add_documents_dao(documents: list[DocumentModel],
+                      user_group: str):
+    """ TODO
+    """
+    vector_store = PGVector(
+        embeddings=embeddings,
+        collection_name=user_group,
+        connection=connection,
+        use_jsonb=True,
+    )
+
+    docs = [
+        Document(page_content=doc.content, metadata=doc.metadata)
+        for doc in documents
+    ]
+
+    vector_store.add_documents(
+        docs,
+        ids=[doc.metadata["id"] for doc in docs]
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    dummy_docs = [
+        DocumentModel(
+            content="The king is dead.".encode('utf-8'),
+            trace_id=uuid4().hex,
+            file_name="P3ty4",
+            metadata={"id": uuid4().hex, "topic": "dummy"}
+        ),
+        DocumentModel(
+            content="Long live the king!".encode('utf-8'),
+            trace_id=uuid4().hex,
+            file_name="P3ty4",
+            metadata={"id": uuid4().hex, "topic": "dummy"}
+        ),
+    ]
+
+    add_documents_dao(documents=dummy_docs, user_group="test")
--- cocoarag/models/documents.py
+++ cocoarag/models/documents.py
@@ -1,9 +1,34 @@
 # models.py
-from entities.documents import Document
+from pydantic import BaseModel, Field, field_validator
+from uuid import UUID, uuid4
 
-class DocumentModel:
-    def __init__(self, id, content):
-        self.document = Document(id=id, content=content)
 
-    def to_dict(self):
-        return self.document.dict()
+class DocumentModel(BaseModel):
+    """ A model representing a document with a unique trace ID,
+    filename, and raw content.
+
+    Attributes:
+        trace_id (UUID): A unique identifier for the document,
+            representing a single user interaction.
+        file_name (str): The name of the file.
+        file_bytes (bytes): The raw content of the file.
+
+    Methods:
+        validate_file_size(v): Validates the size of the file content.
+    """
+    trace_id: UUID = Field(default_factory=uuid4,
+                           frozen=True,
+                           description="UUID represents a single user interaction")
+    file_name: str = Field(description="Filename")
+    content: bytes = Field(description="Raw content")
+    metadata: dict  # TODO: validation & обязательные поля типа названия
+
+    @field_validator("content")
+    @staticmethod
+    def validate_file_size(v):
+        min_size = 1  # 1 byte
+
+        if len(v) < min_size:
+            raise ValueError("File is too small. The minimum allowed file size is 512 bytes.")
+
+        return v
